ba95a62adb2096ae98cfbaa47aac3dcdd56dddab
Meskour <Meskour@DESKTOP-3C5DDI0>
2025-07-09 11:48:12 +0100

    update

--- src/main/java/ma/project/test_technique/TestTechniqueApplication.java
+++ src/main/java/ma/project/test_technique/TestTechniqueApplication.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
     "servicesImpl"
 })
 @EntityScan(basePackages = {"entites"})
-@EnableJpaRepositories(basePackages = {"repository"}) // ← très important
+@EnableJpaRepositories(basePackages = {"repository"}) 
 public class TestTechniqueApplication {
 
     public static void main(String[] args) {
